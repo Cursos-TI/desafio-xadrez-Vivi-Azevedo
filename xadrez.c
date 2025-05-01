75abfb20a6604c75f3818748aabb2770bb80997f
Vivi-Azevedo <viviazevedo2909@gmail.com>
2025-05-01 21:49:06 +0000

    atualização jogo peças de xadrez desafio mestre

--- xadrez.c
+++ xadrez.c
@@ -1,46 +1,75 @@
 #include <stdio.h>
 
+void movtorre (int t) {
+
+    if (t > 0) {
+        printf("Direita\n");
+        movtorre(t-1);
+    }
+
+}
+
+void movbispo (int b) {
+    if (b >0) {
+        printf("Cima, Direita\n");
+        movbispo (b-1);
+
+
+    }
+
+}
+
+void movrainha (int r) {
+    if (r >0){
+        printf ("Esquerda\n");
+        movrainha (r-1);
+
+    }
+
+}
+
+
 int main (){
 
 // Declaração de constantes e variaveis.
     const int torre = 5, bispo = 5, rainha = 8, cavalov =2, cavaloh=1;
-    int i=1 , i2=1 , i3, i4, i5 = 1;
 
     printf("*Movimentação da peça Torre*\n");
-    while (i <= torre){
 
-        printf("Direita\n");
+    movtorre(torre);
 
-        i++;    
-    } 
 
     printf("\n*movimetação da Peça Bispo*\n");
 
-    do {
-        printf("Cima, Direita\n");
-        i2++;
+    movbispo (bispo);
 
-    } while (i2<= bispo);
 
     printf ("\n*Movimentação da peça Rainha*\n");
 
-    for (i3 = 1; i3<=rainha; i3++){
-        printf("Esquerda\n");
-    }
+    movrainha (rainha);
 
 
-    do {
-        printf ("\n*Movimentação da peça Cavalo*\n");
+    printf ("\n*Movimentação da peça Cavalo*\n");
 
-        for(i4 = 1; i4 <=2; i4++) {
-            printf("Baixo\n");
+    for (int c = 1 ; c <= 5; c++) {
+
+        if (c > cavaloh) {
+            break;
         }
 
-        printf("Esquerda\n");
-        i5++;
-    
-    } while (i5<=1);
+        for(int c2 = 1; c2 <= 5; c2++) {
 
+            if (c2 > cavalov) {
+                break;
+            }
+            printf("Cima\n");
+
+        }
+
+        printf("Direita\n");
+
+    }
+       
 
     return 0;
 }
